741406aa7ab026b1cb38e1c0d273c978513ff8ce
choiyounwook <dusdnr2694@naver.com>
2023-02-25 22:29:26 +0900

    remove folder

--- yeongwoo-plate/src/components/FlashMessage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function FlashMessage(props) {
-  return <div>{props.message}</div>;
-}
-
-FlashMessage.defaultProps = {
-  message: "An error occurred",
-};
--- yeongwoo-plate/src/modules/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import stores from "../reducers/Search";
-import detailStore from "../reducers/DetailStore";
-import detailReview from "../reducers/DetailReview";
-
-const rootReducer = combineReducers({ stores,detailStore,detailReview });
-
-export default rootReducer;
